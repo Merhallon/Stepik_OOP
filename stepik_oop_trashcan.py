28c58892f8e02cb09590b5c46ab8a06a1207491c
marina.s <marina.s@ati.su>
2022-10-10 21:34:40 +0400

    trashcan 2 part

--- stepik_oop_trashcan.py
+++ stepik_oop_trashcan.py
@@ -14,6 +14,14 @@
 # печатает фразу «ErrorWriteFileDeleted({name})», если атрибут is_deleted истин, и выходит из метода
 # печатает фразу «ErrorWriteFileTrashed({name})», если атрибут in_trash истин, и выходит из метода
 # печатает фразу «Записали значение {content} в файл {self.name}», если файл не удален и не в корзине
+
+# Далее создайте класс  Trash у которого есть:
+#
+# атрибут класса  content изначально равный пустому списку
+# статик-метод  add, который принимает файл и сохраняет его в корзину: для этого нужно добавить его в атрибут content и проставить файлу атрибут in_trash значение True. Если в метод add передается не экземпляр класса File, необходимо вывести сообщение «В корзину добавлять можно только файл»
+# статик-метод  clear, который запускает процесс очистки файлов в корзине. Необходимо для всех файлов, хранящийся в атрибуте content , в порядке их добавления в корзину вызвать метод файла remove. Атрибут content  после очистки должен стать пустым списком. Сама процедура очистки должна начинаться фразой «Очищаем корзину» и заканчиваться фразой «Корзина пуста»
+# статик-метод  restore, который запускает процесс восстановления файлов из корзины. Необходимо для всех файлов, хранящийся в атрибуте content , в порядке их добавления в корзину вызвать метод файла restore_from_trash. Атрибут content  после очистки должен стать пустым списком.
+# Сама процедура восстановления должна начинаться фразой «Восстанавливаем файлы из корзины» и заканчиваться фразой «Корзина пуста»
 class File:
     def __init__(self, name):
         self.name = name
@@ -45,15 +53,60 @@ class File:
             return print(f'Записали значение {content} в файл {self.name}')
 
 
+class Trash:
+    content = []
 
+    @staticmethod
+    def add(file):
+        if not isinstance(file, File):
+            return print("В корзину добавлять можно только файл")
+        Trash.content.append(file)
+        file.in_trash = True
+
+    @staticmethod
+    def clear():
+        print("Очищаем корзину")
+        for file in  reversed(Trash.content):
+            file.remove()
+        Trash.content = []
+        print('Корзина пуста')
+
+    @staticmethod
+    def restore():
+        print("Восстанавливаем файлы из корзины")
+        for file in reversed(Trash.content):
+            file.restore_from_trash()
+        Trash.content = []
+        print('Корзина пуста')
 
 # f1 = File('puppies.jpg')
 # print(f1.__dict__)  # {'name': 'puppies.jpg', 'in_trash': False, 'is_deleted': False}
 # f1.read()  # Прочитали все содержимое файла puppies.jpg
 # f1.remove()  # Файл puppies.jpg был удален
 # f1.read()  # ErrorReadFileDeleted(puppies.jpg)
-#
+# #
+# f2 = File('cat.jpg')
+# f2.write('hello')  # Записали значение hello в файл cat.jpg
+# f2.remove()  # Файл cat.jpg был удален
+# f2.write('world')  # ErrorWriteFileDeleted(cat.jpg)
+
+f1 = File('puppies.jpg')
 f2 = File('cat.jpg')
-f2.write('hello')  # Записали значение hello в файл cat.jpg
-f2.remove()  # Файл cat.jpg был удален
-f2.write('world')  # ErrorWriteFileDeleted(cat.jpg)
+passwords = File('pass.txt')
+
+f1.read()  # Прочитали все содержимое файла puppies.jpg
+Trash.add(f1)
+f1.read() # ErrorReadFileTrashed(puppies.jpg)
+
+Trash.add(f2)
+Trash.add(passwords)
+Trash.clear() # после этой команды вывод должен быть таким
+# '''
+# Очищаем корзину
+# Файл puppies.jpg был удален
+# Файл cat.jpg был удален
+# Файл pass.txt был удален
+# Корзина пуста
+# '''
+#
+f1.read() # ErrorReadFileTrashed(puppies.jpg)
