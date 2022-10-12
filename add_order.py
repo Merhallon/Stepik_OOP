def4f60e3d4feaf0547161cfe5905664bcc5b195
marina.s <marina.s@ati.su>
2022-10-12 22:49:15 +0400

    order 3 part

--- add_order.py
+++ add_order.py
@@ -14,6 +14,34 @@
 # Если на счете у пользователя не хватает средств, то необходимо вывести фразу  «Не хватает средств на балансе.
 # Пополните счет» и вернуть False.
 # Если средств хватает, списываем с баланса у пользователя указанную сумму и возвращаем True
+
+# И так, создаем класс Cart, который содержит:
+#
+# метод __init__, принимающий на вход экземпляр класса User .
+# Его необходимо сохранить в атрибуте user .
+# Помимо этого метод  должен создать атрибут goods - пустой словарь (лучше использовать defaultdict).
+# Он нужен будет для хранения наших товаров и их количества(ключ словаря - товар, значение - количество).
+# И еще нам понадобится создать защищенный атрибут .__total со значением 0. В нем будет хранится итоговая сумма заказа
+# метод add принимает на вход экземпляр класса Product и необязательный аргумент количество товаров (по умолчанию 1).
+# Метод add  должен увеличить в корзине (атрибут goods) количество указанного товара  на переданное значение количество
+# и пересчитать атрибут self.__total
+# метод remove  принимает на вход экземпляр класса Product и необязательный аргумент количество товаров (по умолчанию 1).
+# Метод remove  должен уменьшить в корзине (атрибут goods) количество указанного товара
+# на переданное значение количество и пересчитать атрибут self.__total. Обратите внимание на то, что количество
+# товара в корзине не может стать отрицательным как и итоговая сумма
+# Cвойство геттер total  , которое возвращает значение self.__total;
+# метод order  должен использовать метод payment  из класса User и передать в него итоговую сумму корзины.
+# В случае, если средств пользователю хватило, вывести сообщение «Заказ оплачен»,
+# в противном случае - «Проблема с оплатой»;
+# метод print_check  печатающий на экран чек. Он должен начинаться со строки
+# ---Your check---
+# Затем выводится состав корзины в алфавитном порядке по названию товара в виде
+# {Имя товара} {Цена товара} {Количество товара} {Сумма}
+# И заканчивается чек строкой
+# ---Total: {self.total}---
+from collections import defaultdict
+
+
 class Product:
     def __init__(self, name, price):
         self.name = name
@@ -47,11 +75,82 @@ class User:
             self.__balance = self.__balance - i
             return True
 
+
+class Cart:
+    def __init__(self, user: User):
+        self.user = user
+        self.goods = {} # ключ словаря - товар, значение - количество
+        self.__total = 0  # итоговая сумма заказа
+
+    def add(self, product: Product, number_of_goods=1):
+        self.goods[product] = self.goods.get(product, 0) + number_of_goods
+        self.__total += product.price * number_of_goods
+
+    def remove(self, product, number_of_goods=1):
+        if self.goods.get(product) <= number_of_goods:
+            self.__total -= product.price * self.goods[product]
+            del self.goods[product]
+        else:
+            self.goods[product] -= number_of_goods
+            self.__total -= product.price * number_of_goods
+
+    @property
+    def total(self):
+        return self.__total
+
+    def order(self):
+        if self.user.payment(self.__total):
+            print("Заказ оплачен")
+        else:
+            print('Проблема с оплатой')
+
+    def print_check(self):
+        print("---Your check---")
+        for i in sorted(self.goods.items(), key=lambda x: x[0].name):
+            print(i[0].name, i[0].price, i[1], i[1] * i[0].price)
+        print(f'---Total: {self.total}---')
+
+
 billy = User('billy@rambler.ru')
-print(billy) # Пользователь billy@rambler.ru, баланс - 0
-billy.deposit(100)
-billy.deposit(300)
-print(billy) # Пользователь billy@rambler.ru, баланс - 400
-billy.payment(500) # Не хватает средств на балансе. Пополните счет
-billy.payment(150)
-print(billy) # Пользователь billy@rambler.ru, баланс - 250
+
+lemon = Product('lemon', 20)
+carrot = Product('carrot', 30)
+
+cart_billy = Cart(billy)
+print(cart_billy.user) # Пользователь billy@rambler.ru, баланс - 0
+cart_billy.add(lemon, 2)
+cart_billy.add(carrot)
+cart_billy.print_check()
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+lemon 20 2 40
+---Total: 70---'''
+cart_billy.add(lemon, 3)
+cart_billy.print_check()
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+lemon 20 5 100
+---Total: 130---'''
+cart_billy.remove(lemon, 6)
+cart_billy.print_check()
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+---Total: 30---'''
+print(cart_billy.total) # 30
+cart_billy.add(lemon, 5)
+cart_billy.print_check()
+''' Печатает текст ниже
+---Your check---
+carrot 30 1 30
+lemon 20 5 100
+---Total: 130---'''
+cart_billy.order()
+''' Печатает текст ниже
+Не хватает средств на балансе. Пополните счет
+Проблема с оплатой'''
+cart_billy.user.deposit(150)
+cart_billy.order() # Заказ оплачен
+print(cart_billy.user.balance) # 20
